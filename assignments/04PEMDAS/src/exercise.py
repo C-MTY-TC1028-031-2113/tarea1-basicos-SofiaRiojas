eccdbcd52828b8a5185a5260cc559c97ddc931af
A01177990 <a01177990@itesm.mx>
2021-08-20 18:18:04 -0500

    20082021

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,22 +1,20 @@
+import math 
 def main():
-    from math import sqrt
-    pass
-a=4 
-b=5
-#oper1 = (2*(3/4)) + (4(2/3))-(-3/(1/5))+(5(1/2))
-#print (oper1)
-oper2 = (2*math.sqrt((35)**2)) + (4*math.sqrt((36)**3)) - (6*math.sqrt((49)**2))
-print = (oper2) 
-oper3= oper3= (a**3 + 2*b**2) / 4*a
-print = (oper3)
-oper4= ((2*(a+b)**2) + (4*(a-b)**2)) / (a*(b**2))
-print = (oper4)
-oper5 = math.sqrt(((a+b)**2+2**(a+b)) / (2*a+2*b)**2)
-print = (oper5)
+    a=4
+    b=5
+    oper1 = round((2*(3/4)) + (4*(2/3))-(3*(1/5))+(5*(1/2)))
+    print (oper1)
+    oper2= round((2*math.sqrt((35)**2)) + (4*math.sqrt((36)**3)) - (6*math.sqrt((49)**2)))
+    print (oper2)
+    oper3 =(a**3 + 2*b**2) / (4*a)
+    print (oper3)
+    oper4= ((2*(a+b)**2) + (4*(a-b)**2)) / (a*(b**2))
+    print (oper4)
+    oper5 = math.sqrt((a+b)**2 + 2**(a+b)) / (2*a+2*b)**2
+    print (oper5)
+    
 
 
-
-#profe, lo cheque en Thonny y se me dejo, pero aqui no me sirve el "import math, tambien en la ultima intente de varias formas y no de daba."
 
 if __name__ == '__main__':
     main()
