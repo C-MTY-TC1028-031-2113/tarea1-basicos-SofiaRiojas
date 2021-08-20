959ff7508c0211b93dda8b4e8963ee30e36e6ed6
A01177990 <a01177990@itesm.mx>
2021-08-20 15:15:53 -0500

    absxhbwouidbxo

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -7,8 +7,10 @@ b=5
 #print (oper1)
 oper2 = (2*math.sqrt((35)**2)) + (4*math.sqrt((36)**3)) - (6*math.sqrt((49)**2))
 print = (oper2) 
-oper3= (a**(3))+ (2*(b**(2))) / 4*a
+oper3= oper3= (a**3 + 2*b**2) / 4*a
 print = (oper3)
+oper4= ((2*(a+b)**2) + (4*(a-b)**2)) / (a*(b**2))
+print = (oper4)
 
 
 
